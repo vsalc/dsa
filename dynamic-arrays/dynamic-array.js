0a6a6a8a37c5e98a3636aad41da6109c15aaaf84
Victor Salcedo <vsalc@proton.me>
2026-01-01 05:33:53 -0500

    Boilerplate added for dynamic array class and tests.

---
+++ dynamic-arrays/dynamic-array.js
@@ -0,0 +1,18 @@
+/*
+ * File: dynamic-array.js
+ * Description: Implementing basic data structures and algorithms in 
+ *              JavaScript.
+ * Author: Victor Salcedo
+ * Date: January 1, 2026
+ * Contact: vsalc@proton.me
+ */
+
+class DynamicArray {
+  constructor (arr = []) {
+    this.arr = arr
+  }
+
+  // Todo: everything
+}
+
+export default DynamicArray
---
+++ dynamic-arrays/test-dynamic-array.js
@@ -0,0 +1,12 @@
+/*
+ * File: test-dynamic-array.js
+ * Description: Unit tests for dynamic-array.js.
+ * Author: Victor Salcedo
+ * Date: January 1, 2026
+ * Contact: vsalc@proton.me
+ */
+import test from "node:test"
+import assert from "node:assert/strict"
+import DynamicArray from "./dynamic-array.js"
+
+// Todo: everything
